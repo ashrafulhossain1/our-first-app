f6701c9792086c1b582904f6d71c307dcc04f180
ashrafulhossain1 <ahmeadashraful@gmail.com>
2025-03-09 23:34:34 +0600

    after accept demo 2

--- src/app/Components/Demo2/Demo2.jsx
+++ src/app/Components/Demo2/Demo2.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Demo2 = () => {
     return (
         <div>
-            <h2 className='bg-green-500 text-6xl py-4'>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
+            <h2 className='bg-red-500 text-6xl py-4 border '>Demo-2</h2>
         </div>
     );
 };
